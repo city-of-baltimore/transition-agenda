7c8eb28cfd2f4dd9dcd3a88236843ccedba240de
Brendan Hellweg <brendan.hellweg@baltimorecity.gov>
2020-12-11 19:28:22 -0500

    Update transition-agenda.R

--- transition-agenda.R
+++ transition-agenda.R
@@ -1,9 +1,22 @@
+#install relevant packages
 install.packages("shiny")
 install.packages("tidyverse")
+install.packages("googlesheets4")
 
+#load packages
 library(shiny)
 library(tidyverse)
+library(googlesheets4)
 
+
+#set up google sheets access
+pg1 <- read_sheet("https://docs.google.com/spreadsheets/d/1lkTa3zDSJ-aO0c0XxfocQP0cGPRQr68YYYwYPPBp3qo/edit?usp=sharing", sheet = 1)
+pg2 <- read_sheet("https://docs.google.com/spreadsheets/d/1lkTa3zDSJ-aO0c0XxfocQP0cGPRQr68YYYwYPPBp3qo/edit?usp=sharing", sheet = 2)
+pg3 <- read_sheet("https://docs.google.com/spreadsheets/d/1lkTa3zDSJ-aO0c0XxfocQP0cGPRQr68YYYwYPPBp3qo/edit?usp=sharing", sheet = 3)
+pg4 <- read_sheet("https://docs.google.com/spreadsheets/d/1lkTa3zDSJ-aO0c0XxfocQP0cGPRQr68YYYwYPPBp3qo/edit?usp=sharing", sheet = 4)
+
+
+#produce basic shell for the site
 ui <- pageWithSidebar(
   
   
