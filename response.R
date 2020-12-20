58f699001868904388e2d807f9c2a6378519c5bb
Brendan Hellweg <brendan.hellweg@baltimorecity.gov>
2020-12-20 13:43:08 -0500

    Update response.R

--- response.R
+++ response.R
@@ -2,7 +2,6 @@
 loadData <- function() {
   files <- list.files(file.path(responsesDir), full.names = TRUE)
   data <- lapply(files, read.csv, stringsAsFactors = FALSE)
-  #data <- dplyr::rbind_all(data)
   data <- do.call(rbind, data)
   data
 }
