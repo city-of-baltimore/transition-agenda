344d08563d4dddaacf03e0d9dc5a87b72c236ea7
Brendan Hellweg <brendan.hellweg@baltimorecity.gov>
2020-12-20 13:44:51 -0500

    Update response.R

--- response.R
+++ response.R
@@ -1,9 +1,18 @@
 # load all responses into a data.frame
-loadData <- function() {
-  files <- list.files(file.path(responsesDir), full.names = TRUE)
-  data <- lapply(files, read.csv, stringsAsFactors = FALSE)
-  data <- do.call(rbind, data)
-  data
-}
 
-loadData()
+  #-----------------------------
+  
+  # Create function to bind all responses
+
+    loadData <- function() {
+      files <- list.files(file.path(responsesDir), full.names = TRUE)
+      data <- lapply(files, read.csv, stringsAsFactors = FALSE)
+      data <- do.call(rbind, data)
+      data
+    }
+
+  #---------------------------
+    
+  # Run function (this is just to show that it works, the final version should write to some location)
+  
+    loadData()
