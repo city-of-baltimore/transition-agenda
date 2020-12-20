352a908dbae15bde3d1e34db36664a9308975365
Brendan Hellweg <brendan.hellweg@baltimorecity.gov>
2020-12-20 17:49:33 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,9 +1,27 @@
+library(pacman)
+library(ggplot2)
+library(googlesheets4)
+library(formattable)
 library(shiny)
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(DT)
+library(shinyjs)
+library(rsconnect)
+
+#pacman::p_load(ggplot2, gfonts, googlesheets4, 
+ #              formattable, shiny, tidyverse,
+  #             lubridate, readxl, DT, shinyjs,
+   #            rsconnect)
+
+
+#setwd("C:/Users/brend/OneDrive/Documents/GitHub/transition-agenda")
 
 source('preset.R',local = T)
 source('survey.R',local = T)
-source('ui.R',local = T)
 source('server.R',local = T)
+source('ui.R',local = T)
 
 shinyApp(
   ui = ui,
