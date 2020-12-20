bd0bbd3a034a68bd129e77386bf8493602dbb58f
Brendan Hellweg <brendan.hellweg@baltimorecity.gov>
2020-12-20 13:41:59 -0500

    Create response.R

---
+++ response.R
@@ -0,0 +1,10 @@
+# load all responses into a data.frame
+loadData <- function() {
+  files <- list.files(file.path(responsesDir), full.names = TRUE)
+  data <- lapply(files, read.csv, stringsAsFactors = FALSE)
+  #data <- dplyr::rbind_all(data)
+  data <- do.call(rbind, data)
+  data
+}
+
+loadData()
