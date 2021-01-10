f7dcf785430ef328faa74489d106289218e022c5
Brendan Hellweg <brendan.hellweg@baltimorecity.gov>
2021-01-10 18:12:17 -0500

    Update server.R

--- server.R
+++ server.R
@@ -14,23 +14,6 @@
     server <- function(input, output, session){
 
   #------------------------------------
-      
-  # Dynamic titles for timeline and progress bars
-      
-    # Text for timeline vis title. Note this text will treat the first day in 
-    # the tbDays dataframe as the first day of the 100 Days of Action. If the 
-    # first day is changed, the source data should be filtered to exclude days 
-    # preceeding the first day.
-    timelineText <- paste0("The 100 Days of Action began on 12/8/20. Today is day ",
-      ifelse(which(tbDays$Date == Sys.Date())<=100, 
-        which(tbDays$Date == Sys.Date()),
-        paste0("It ended on ", 
-          gsub("/0", "/", strftime(tbDays$Date[100], "%m/%d/%y")))), ".")
-    
-    progressText <- paste0("There are ", actionsTotal, " actions planned in total. ",
-      actionsComplete, " are complete, and ", actionsInProgress, " are in progress.")
-  
-  #------------------------------------
     
   # Table with progress on all priority areas and actions
   
