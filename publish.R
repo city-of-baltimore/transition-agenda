0c65bfc14e4d5b9072aad2d52b674e576585aeb4
Brendan Hellweg <59033479+bhellweg@users.noreply.github.com>
2021-01-29 18:46:48 -0500

    Delete publish.R

--- publish.R
+++
@@ -1,6 +0,0 @@
-rsconnect::setAccountInfo(name='baltopi',
-                          token='76DB30C93B45ACE665E1DBA6E9E3255B',
-                          secret='k1XAlNcc1T15vyrYT7cVcvCbXrS2Iw5WT1rnm7+H')
-
-rsconnect::deployApp("C:/Users/brend/OneDrive/Documents/GitHub/transition-agenda")
-
